73f7c2aa17b791911045cf754dd9c81c3ded7160
xuqiushuo <18351927901@163.com>
2018-11-22 16:03:10 +0800

    change author

--- src/lengthOfLongestSubstring.java
+++ src/lengthOfLongestSubstring.java
@@ -1,4 +1,8 @@
 //给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度
+/**
+ *  * @author leetcode
+ *   * @date 2018/11/21 22:02
+ *    */
 public class Solution {
     public int lengthOfLongestSubstring(String s) {
         int n = s.length();
